53e462517ae8e516f3fb41f2ad3f22e0c71b831a
joegamal <yusufmagamalth@gmail.com>
2024-09-09 01:54:33 +0300

    update with arr length

--- array.cpp
+++ array.cpp
@@ -4,11 +4,20 @@ int main(){
 
     int arr[5] = {1,2,3,4,5}; //this is how to initialize an array.
 
+    int x = 5;
+    int* att = new int[x]; //this is how to initialize a dynamic array.
+
     for(int i = 0; i < 5; i++){ // this is how to iterate and to access the elements of the array.
         cout<<arr[i]<<" ";
     }
     cout<<endl;
     cout<<arr; // this prints the address of the first element of the array.
+
+
+     int length = sizeof(arr) / sizeof(arr[0]); // this is how to print the length of the array.
+    cout << "The length of the array is: " << length << endl;
+
+
     cout<<endl;
     return 0;
 }
